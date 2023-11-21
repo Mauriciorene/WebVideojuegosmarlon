2b9c88df2e3e0dcea530650ef48d031f64dbc053
Mauriciorene <rubiom831@gmail.com>
2023-11-20 21:46:33 -0600

    cuadro de informacion

--- frontend-video-juegos-marlon-react/src/pages/About.js
+++ frontend-video-juegos-marlon-react/src/pages/About.js
@@ -1,6 +1,5 @@
-// About.js
 import React, { useEffect } from 'react';
-import { Container, } from 'react-bootstrap';
+import { Container } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 import '../styles/App.css';
@@ -11,23 +10,32 @@ function About({ Rol }) {
     // Cambia el color del texto del párrafo a blanco
     const parrafo = document.querySelector('p');
     if (parrafo) {
-      parrafo.style.color = 'white';
+      parrafo.style.color = 'black';
     }
   }, []); // El segundo parámetro [] asegura que el efecto se ejecute solo una vez al montar el componente
 
-
-  return(
+  return (
     <div>
-      <Header Rol={ Rol } />
+      <Header Rol={Rol} />
 
-      <Container className="margen-contenedor"></Container>  
-      <Link to="/Inicio">Ir a inicio</Link>
-                <p color=''>
-                    "Somos una Sala de videojuegos apasionada por la cultura gamer. 
-                    Ofrecemos una amplia selección de títulos para todas las plataformas, desde clásicos hasta lanzamientos recientes. 
-                    Nuestro local esta listo para asesorarte y brindarte la mejor experiencia de compra. 
-                    ¡Únete a la diversión y explora nuestro mundo de videojuegos!"
-                </p>                
+      <Container className="margen-contenedor">
+        <Link to="/Inicio">Ir a inicio</Link>
+        <div
+          style={{
+            backgroundColor: 'white',
+            padding: '20px',
+            border: '1px solid #ccc',
+            borderRadius: '8px',
+          }}
+        >
+          <p style={{ color: 'black' }}>
+            "Somos una Sala de videojuegos apasionada por la cultura gamer.
+            Ofrecemos una amplia selección de títulos para todas las plataformas, desde clásicos hasta lanzamientos recientes.
+            Nuestro local está listo para asesorarte y brindarte la mejor experiencia de compra.
+            ¡Únete a la diversión y explora nuestro mundo de videojuegos!"
+          </p>
+        </div>
+      </Container>
     </div>
   );
 }
